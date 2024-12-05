3d42b9ba0ce591ef159190a46597ddadf04469b9
Gabriel Fleury <55366345+ga-fleury@users.noreply.github.com>
2024-12-05 15:30:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## This is a starter file.
+
+Minimal project setup with Vite + TS + Tailwind
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+});
